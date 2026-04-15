3e745dcfa5b261f1c171e56c89d0a8b8c808230c
Octavio <octiduarte@hotmail.com>
2026-04-15 20:55:02 -0300

    chore: comment out pricing section on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,7 +137,7 @@ export default function Home() {
 
       <HowItWorks />
       <Playbooks />
-      <Pricing />
+      { /* <Pricing /> */ }
 
       <footer className="bg-surface-soft border-t border-border-default py-6 px-6 text-center">
         <p className="text-sm text-text-muted/80">
